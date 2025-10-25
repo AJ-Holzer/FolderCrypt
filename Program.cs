20a000e27884c2a23102d6d5208408231ae52f3e
AJ-Holzer <axel123.holzer@gmail.com>
2025-10-25 10:27:37 +0200

    Add `EncryptionHandler` class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ============================== //
+//  Copyright (c) AJ-Holzer       //
+//  SPDX-License-Identifier: MIT  //
+// ============================== //
+
+
+namespace FolderCrypt;
+
+using FolderCrypt.crypto;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Check if one element is provided in args
+        if (args.Length != 1)
+            throw new ArgumentException("A file/folder path must be specified to encrypt!");
+
+        // Get path
+        string path = args[0];
+    }
+}
---
+++ crypto/aes_gcm.cs
@@ -0,0 +1,36 @@
+// ============================== //
+//  Copyright (c) AJ-Holzer       //
+//  SPDX-License-Identifier: MIT  //
+// ============================== //
+
+
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FolderCrypt.crypto;
+
+static class EncryptionHandler
+{
+    public static byte[] Encrypt(string plaintext, byte[] key, out byte[] nonce, out byte[] tag)
+    {
+        nonce = RandomNumberGenerator.GetBytes(count: 12);  // 96-bit nonce (standard)
+        byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+        byte[] ciphertext = new byte[plaintextBytes.Length];
+        tag = new byte[16];  // 128 bit tag
+
+        using AesGcm aes = new(key, tag.Length);
+        aes.Encrypt(nonce: nonce, plaintext: plaintextBytes, ciphertext: ciphertext, tag: tag);
+
+        return ciphertext;
+    }
+
+    public static string Decrypt(byte[] ciphertext, byte[] key, byte[] nonce, byte[] tag)
+    {
+        byte[] plaintextBytes = new byte[ciphertext.Length];
+
+        using AesGcm aes = new(key, tag.Length);
+        aes.Decrypt(nonce: nonce, ciphertext: ciphertext, tag: tag, plaintext: plaintextBytes);
+
+        return Encoding.UTF8.GetString(plaintextBytes);
+    }
+}
